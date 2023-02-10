fc82f0a73fd4baccb13623a061b29bf681d71c45
juan-aga <juan-aga@student.42malaga.com>
2023-02-10 12:19:02 +0100

    modify minishell.h to add struct
    added struct ms and cmdarg

--- include/minishell.h
+++ include/minishell.h
@@ -1,1 +1,20 @@
-/* empty */
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+typedef struct s_cmdlst
+{
+	struct s_cmdlst	*next;
+	char		*path;
+	char		**arg;
+}	t_cmdlst;
+
+typedef struct s_ms 
+{
+	struct s_cmdlst	*command;
+	int		num_com;
+	int		fd_in;
+	int		fd_out;
+	t_pid		pid;
+}	t_ms;
+
+
