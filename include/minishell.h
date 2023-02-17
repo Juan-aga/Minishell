f9557b4fe1a8d04914ab0e6dd69629a8d9501ac8
juan-aga <juan-aga@student.42malaga.com>
2023-02-17 20:14:35 +0100

    FIX: implemented builtin in cmdlst
    builtins works with exec, some procces in fork other in main procces.

--- include/minishell.h
+++ include/minishell.h
@@ -60,6 +60,8 @@ void		ft_unset(char *str, t_ms *ms);
  * 		if it exist it returns the position in env[ ]
  * 		else it returns  0;						*/
 int			ft_check_env(char *str, t_ms *ms, char c);
+/*		builtin utils				*/
+int			ft_is_builtin(t_ms *ms, t_cmdlst *tmp);
 
 /*		enviroment utils				*/
 void		ft_free_array(char **str, int i);
--- src/exec.c
+++ src/exec.c
@@ -5,7 +5,8 @@
 
 static int	ft_childs_pip(t_ms *ms, t_cmdlst *tmp);
 static void	ft_childs_exe(t_ms *ms, t_cmdlst *tmp);
-static void	ft_child_infile(t_ms *ms, t_cmdlst *tmp);
+static void	ft_child_redir_file(t_ms *ms, t_cmdlst *tmp);
+static void	ft_accept_redirections(t_ms *ms, t_cmdlst *tmp);
 
 void	ft_exec(t_ms *ms)
 {
@@ -41,18 +42,13 @@ static int	ft_childs_pip(t_ms *ms, t_cmdlst *tmp)
 	pid_t	pros;
 	int		status;
 
+	status = ft_is_builtin(ms, tmp);
+	if (status)
+		return (ms->exit_status);
 	pros = fork();
 	if (!pros)
 	{
-		ft_child_infile(ms, tmp);
-		if (tmp->fd_out_file)
-			ms->pipe[2 * ms->exe + 1] = tmp->fd_out;
-		else if (!tmp->next)
-			ms->pipe[2 * ms->exe + 1] = tmp->fd_out;
-		if (!ms->exe)
-			ft_dup(tmp->fd_in, ms->pipe[2 * ms->exe + 1]);
-		else
-			ft_dup(ms->pipe[2 * ms->exe - 2], ms->pipe[2 * ms->exe + 1]);
+		ft_child_redir_file(ms, tmp);
 		ft_childs_exe(ms, tmp);
 		exit(-1);
 	}
@@ -62,7 +58,7 @@ static int	ft_childs_pip(t_ms *ms, t_cmdlst *tmp)
 	return (0);
 }
 
-static void	ft_child_infile(t_ms *ms, t_cmdlst *tmp)
+static void	ft_child_redir_file(t_ms *ms, t_cmdlst *tmp)
 {
 	if (tmp->fd_in_file)
 	{
@@ -73,11 +69,20 @@ static void	ft_child_infile(t_ms *ms, t_cmdlst *tmp)
 		}
 		ms->pipe[2 * ms->exe] = tmp->fd_in;
 	}
+	if (tmp->fd_out_file)
+		ms->pipe[2 * ms->exe + 1] = tmp->fd_out;
+	else if (!tmp->next)
+		ms->pipe[2 * ms->exe + 1] = tmp->fd_out;
+	if (!ms->exe)
+		ft_dup(tmp->fd_in, ms->pipe[2 * ms->exe + 1]);
+	else
+		ft_dup(ms->pipe[2 * ms->exe - 2], ms->pipe[2 * ms->exe + 1]);
 }
 
 static void	ft_childs_exe(t_ms *ms, t_cmdlst *tmp)
 {
 	ft_close_pipe(ms);
+	ft_accept_redirections(ms, tmp);
 	close(ms->pipe[2 * ms->exe - 2]);
 	ft_get_path(ms, tmp);
 	if (!tmp->path)
@@ -87,4 +92,22 @@ static void	ft_childs_exe(t_ms *ms, t_cmdlst *tmp)
 		exit(-1);
 	}
 	execve(tmp->path, tmp->arg, ms->env);
+}
+
+static void	ft_accept_redirections(t_ms *ms, t_cmdlst *tmp)
+{
+	if (!ft_strncmp("env", tmp->arg[0], 4))
+	{
+		ft_env(ms);
+		exit (0);
+	}
+	else if (!ft_strncmp("pwd", tmp->arg[0], 4))
+	{
+		ft_pwd(ms);
+		exit (0);
+	}
+	else if (!ft_strncmp("echo", tmp->arg[0], 5))
+	{	
+		exit (0);
+	}
 }
