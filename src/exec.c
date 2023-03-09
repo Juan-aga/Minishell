250febf4aa0b9b0c7f2ef672029db2c02ac7dfa2
juan-aga <juan-aga@student.42malaga.com>
2023-03-09 09:42:21 +0100

    MOD: error msg in exec.

--- src/exec.c
+++ src/exec.c
@@ -73,10 +73,8 @@ static void	ft_child_redir_file(t_ms *ms, t_cmdlst *tmp)
 	if (tmp->fd_in_file)
 	{
 		if (tmp->fd_in < 0)
-		{
-			ft_putstr_fd(" No such file or directory\n", 2);
-			exit(-1);
-		}
+			ft_error_file(tmp->arg, tmp->fd_in_file, \
+				": No such file or directory\n");
 		ms->pipe[2 * ms->exe] = tmp->fd_in;
 	}
 	if (tmp->fd_out_file)
@@ -96,11 +94,7 @@ static void	ft_childs_exe(t_ms *ms, t_cmdlst *tmp)
 	close(ms->pipe[2 * ms->exe - 2]);
 	ft_get_path(ms, tmp);
 	if (!tmp->path)
-	{
-		ft_putstr_fd(": command not found\n", 2);
-		ft_free_array(tmp->arg, 0);
-		exit(-1);
-	}
+		ft_error_exe(tmp->arg, ": command not found\n");
 	ft_envlst_to_env(ms);
 	execve(tmp->path, tmp->arg, ms->env);
 	ft_free_array(ms->env, 0);
