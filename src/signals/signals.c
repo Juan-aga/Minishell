ec9f613005097ff39b2d04af94a0041a985f45eb
[Juan A.] <juan-aga@student.42.malaga.com>
2023-03-31 21:10:53 +0200

    FIX: signals, don't show ^

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -2,12 +2,25 @@
 #include "minishell.h"
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <termios.h>
 
 extern t_ms	*g_ms;
 
 /* the strange "\b\b  \b\b" is to delete the \^ token that CTRL+\ prints */
+//	else if (sig == SIGQUIT)
+//		ft_printf("\b\b  \b\b");
+
+/*	fix visual CTRL+ with termios.
+ *	&= ~	: it's disable the attribute.
+ *	ECHOCTL	: reference to CTRL char.
+ *	TCSANOW	: change the attribute immediately.
+ *	termios C_LFLAG	: refers to local modes.
+ */
+
 void	ft_sigint(int sig)
 {
+	struct termios	prompt;
+
 	if (sig == SIGINT)
 	{
 		ft_printf("\n");
@@ -17,8 +30,9 @@ void	ft_sigint(int sig)
 		g_ms->exit_status = 127 + sig;
 		signal(SIGINT, ft_sigint);
 	}
-	else if (sig == SIGQUIT)
-		ft_printf("\b\b  \b\b");
+	tcgetattr(STDIN_FILENO, &prompt);
+	prompt.c_lflag &= ~ECHOCTL;
+	tcsetattr(STDIN_FILENO, TCSANOW, &prompt);
 }
 
 void	ft_sigint_proc(int sig)
